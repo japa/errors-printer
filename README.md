ec6086f6a27e95782a60c7b2930df1088f30eab6
Harminder Virk <virk.officials@gmail.com>
2022-01-19 15:48:31 +0530

    feat: initial working version

---
+++ README.md
@@ -0,0 +1,22 @@
+# @japa/errors-printer
+> Tagline
+
+[![github-actions-image]][github-actions-url] [![npm-image]][npm-url] [![license-image]][license-url] [![typescript-image]][typescript-url]
+
+A short brief
+
+## Installation
+
+## Usage
+
+[github-actions-image]: https://github.com/errors-printer/actions/workflows/test.yml
+[github-actions-url]: https://img.shields.io/github/workflow/status/errors-printer/test?style=for-the-badge "github-actions"
+
+[npm-image]: https://img.shields.io/npm/v/@japa/errors-printer.svg?style=for-the-badge&logo=npm
+[npm-url]: https://npmjs.org/package/@japa/errors-printer "npm"
+
+[license-image]: https://img.shields.io/npm/l/@japa/errors-printer?color=blueviolet&style=for-the-badge
+[license-url]: LICENSE.md "license"
+
+[typescript-image]: https://img.shields.io/badge/Typescript-294E80.svg?style=for-the-badge&logo=typescript
+[typescript-url]:  "typescript"
---
+++ src/Printer/index.ts
@@ -0,0 +1,84 @@
+/*
+ * @japa/errors-printer
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Youch from 'youch'
+import { inspect } from 'util'
+import forTerminal from 'youch-terminal'
+import { diff as jestDiff } from 'jest-diff'
+import { logger, icons } from '@poppinss/cliui'
+
+/**
+ * Print test runner errors
+ */
+export class ErrorsPrinter {
+  /**
+   * Returns human readable message for error phase
+   */
+  private getPhaseTitle(phase: string) {
+    switch (phase) {
+      case 'setup':
+        return 'Setup hook'
+      case 'setup:cleanup':
+        return 'Setup hook cleanup function'
+      case 'teardown':
+        return 'Teardown hook'
+      case 'teardown:cleanup':
+        return 'Teardown hook cleanup function'
+    }
+  }
+
+  /**
+   * Pretty print the error to the console
+   */
+  public async printError(error: any) {
+    const { actual, expected } = error
+
+    /**
+     * Assertion error
+     */
+    if (actual && expected) {
+      const diff = jestDiff(expected, actual, {
+        expand: true,
+        includeChangeCounts: true,
+        compareKeys: () => 0, // Preserves keys order
+      })
+
+      if (!diff || diff.includes('Comparing two different types of values.')) {
+        console.log(logger.colors.red(`    Assertion Error: ${error.message}`))
+        console.log(logger.colors.green('Expected'))
+        console.log(inspect(expected, { colors: true }))
+
+        console.log(logger.colors.green('Actual'))
+        console.log(inspect(actual, { colors: true }))
+      } else {
+        console.log(diff)
+      }
+
+      return
+    }
+
+    const jsonResponse = await new Youch(error, {}).toJSON()
+    console.log(forTerminal(jsonResponse, { prefix: '  ', hideErrorTitle: true }))
+    console.log()
+  }
+
+  /**
+   * Print summary errors
+   */
+  public async printErrors(label: string, errors: { phase: string; error: any }[]) {
+    for (let { phase, error } of errors) {
+      console.log(logger.colors.red(`${icons.cross} ${label}`))
+      if (phase !== 'test') {
+        console.log(`  ${logger.colors.red(`(${this.getPhaseTitle(phase)})`)}`)
+      }
+
+      await this.printError(error)
+    }
+  }
+}
